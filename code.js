78585d2d871ceaca7f97b83528ccc852b939c49d
katacho <75933577+katacho@users.noreply.github.com>
2020-12-16 23:02:10 +0900

    Update code.js

--- code.js
+++ code.js
@@ -1,186 +1,162 @@
-@font-face{
-    font-family: 'Juliamo';
-    src: url('https://dl.dropboxusercontent.com/s/5me44aeb19mxm6e/juliamo.woff') format('woff');
-}
+'use strict';
 
-button, input, select, textarea {
-    font-family : inherit;
-    letter-spacing: inherit;
-    font-size : 100%;
-}
+document.addEventListener('DOMContentLoaded', function () {
 
-*{
-    color:#414141;
-    letter-spacing: 1px;
-}
+    let readyDiv = document.getElementById('readyDiv');
+    let quizDiv = document.getElementById('quizDiv');
 
+    let startButton = document.getElementById('startButton');
+    let answerButton = document.getElementById('answerButton');
+    let quizText = document.getElementById('quizText');
+    let wordData = document.getElementById('wordData');
+    let inputBox = document.getElementById('inputBox');
+    let inputForm = document.getElementById('inputForm');
+    let juliCheck = document.getElementById('juliCheck');
+    let esCheck = document.getElementById('esCheck');
+    let checkIcon1 = document.getElementById('checkIcon1');
+    let checkIcon2 = document.getElementById('checkIcon2');
+    let answerText = document.getElementById('answerText');
+    let resultDiv = document.getElementById('resultDiv');
+    let retryButton = document.getElementById('retryButton');
+    let newButton = document.getElementById('newButton');
+    
 
+    let dictionary = [];
+    let quiz = [];
+    let wrong = false;//現在の問題で間違えたか
+    let answerCount = 0;    //現在連続1回だけ正解している単語
+    const sub = {
+        'ĉ': 'c', 'ĝ': 'g', 'ĥ': 'h', 'ĵ': 'j', 'ŝ': 's', 'Ĉ': 'C', 'Ĝ': 'G', 'Ĥ': 'H', 'Ĵ': 'J', 'Ŝ': 'S'
+    };
 
-li{
-    list-style: none;
-    padding:0;
-}
+    const googleIconName = { unchecked: 'check_box_outline_blank', checked: 'check_box' };
 
+    function convertToEs(str) {//代用表記を変換
+        let newStr = str + '';
+        for (let key in sub) {
+            newStr = newStr.replace(new RegExp(sub[key] + 'x' + '|' + sub[key] + '\\^', 'g'), key);
+            //newStr = newStr.replace(/cx|c\^/g, 'ĉ');
+        }
+        return newStr;
+    }
 
-ul{
-    padding:0;
-    margin:0;
-}
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
 
-#mainContainer{
-    text-align: center;
-    margin:0 20px;
-    height:800px;
-}
-
-#topContainer{
-    height:100px;
-}
-
-#topContainer h1{
-    font-weight: bold;
-    font-size:50px;
-    line-height: 75px;
-    margin:0;
-    padding:20px 0 0;
-}
-
-#mainContainer h2{
-    font-weight: normal;
-    font-size:18px;
-    margin:0;
-    line-height: 27px;
-}
-
-#description h3{
-    border-bottom: 1px solid;
-    font-weight: normal;
-    font-size:16px;
-    margin:20px auto 10px;
-    line-height: 24px;
-    width:90px;
-}
-
-#description p{
-    margin:0 0 30px;
-    font-size:14px;
-    line-height: 21px;
-}
-
-#wordData{
-    width: 200px;
-    height: 200px;
-    padding: 5px;
-    border-radius: 5px;
-    font-size:14px;
-    line-height: 21px;
-    margin:0 auto;
-    display: block;
-}
-
-#startButton{
-    margin:10px 0 0;
-    height: 50px;
-    width:80px;
-    font-size:16px;
-    line-height: 24px;
-}
-
-#bottomContainer{
-    margin:0;
-    width:100%;
-    height:150px;
-}
-
-#checkBox{
-    padding:40px 0 0;
-    margin:0 0 20px;
-    font-size:20px;
-    height:30px;
-    line-height: 45px;
-}
-
-#dsc2{
-    font-size:14px;
-    line-height: 21px;
-}
-
-#quizContainer{
-    height:530px;
-    margin:0;
-}
+    function changeFont(name) {
+        document.body.style.fontFamily = name;
+    }
+    const shuffle = ([...array]) => {
+        for (let i = array.length - 1; i >= 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    }
 
 
+    class Word {
+        constructor([term, meaning]) {
+            this.term = term;
+            this.meaning = meaning;
+            this.count = 0; //連続正解の回数
+        }
+    }
 
-#quizText{
-    padding:100px 0 0;
-    margin:0;
-    font-size:50px;
-    line-height:50px;
-    height:50px;
-}
+    function updateDisplay() {
+        answerText.style.visibility = 'hidden';
+        inputBox.value = '';
+        quizText.textContent = quiz[0].meaning;
+        answerText.textContent = quiz[0].term;
+        if (quiz[0].count == 0) {
+            checkIcon1.innerHTML = googleIconName['unchecked'];
+            checkIcon2.innerHTML = googleIconName['unchecked'];
+        } else {
+            checkIcon1.innerHTML = googleIconName['checked'];
+            checkIcon2.innerHTML = googleIconName['unchecked'];           
+        }
+    }
 
-#answerText{
-    margin:20px 0 50px;
-    font-size:40px;
-    height:40px;
-    visibility:hidden;
-}
+    function judge() {
+        if (esCheck.checked === true) {
+            return (convertToEs(inputBox.value) === quiz[0].term);
+        } else {
+            return (quiz[0].term === inputBox.value);
+        }
+    }
 
-#answerButton{
-    box-sizing: border-box;
-    height:50px;
-    width:100px;
-    margin:0;
-}
+    startButton.onclick = function () {
+        readyDiv.style.display = 'none';
+        quizDiv.style.display = 'inline';
 
-#inputBox{
-    height:60px;
-    width:200px;
-    font-size:30px;
-    line-height:30px;
-    margin:0;
-    border-right:none;
-    border-left:none;
-    border-top:none;
-    border-bottom: #929090 solid 1px;
-    outline: none;
-    transition-duration:0.1s;
-}
+        //テキストエリアから辞書を作成
+        for (let data of wordData.value.split('\n')) {
+            dictionary.push(new Word(data.split(/\s+/)));
+        }
 
-#inputBox:focus{
-    border-bottom: #333333 solid 3px;
-}
+        //問題作成
+        quiz = shuffle(dictionary);
+        shuffle(quiz);
+        updateDisplay();
+    }
 
-#checkIcons{
-    margin:30px 0 0;
+    inputForm.onsubmit = function () {
+        //答えの判定
+        if (wrong === false) {
+            if (judge() === true) {
+                quiz[0].count++;
+                if (quiz[0].count === 2) {
+                    answerCount--;
+                } else {
+                    quiz.splice(quiz.length - getRandomInt(answerCount), 0, quiz[0]);
+                    answerCount++;
+                }
+                quiz.shift();
+                if (quiz.length === 0) {
+                    //クリアしたときの処理
+                    resultDiv.style.display = 'block';
+                    quizDiv.style.display = 'none';
+                } else {
+                    updateDisplay();
+                }
+            } else {
+                if (quiz[0].count === 1) {
+                    quiz[0].count = 0;
+                    updateDisplay();
+                    answerCount--;
+                }
+                wrong = true;
+                answerText.style.color='#cc4646';
+                answerText.style.visibility = 'visible';
+                answerText.animate({ opacity: [0, 1] },300);
+            }
+        } else {
+            if (judge() === true) {
+                if (quiz.length >= 9) {
+                    quiz.splice(9 + getRandomInt(Math.min(7, quiz.length - 8)), 0, quiz[0]); //8番目から15番目のどれかへ移動
+                } else {
+                    quiz.splice(getRandomInt(quiz.length) + 1, 0, quiz[0]);
+                }
+                quiz.shift();
+                wrong = false;
+                updateDisplay();
+            }
+        }
+        return false;
+    }
 
-}
+    juliCheck.onchange = function () {
+        if (juliCheck.checked) {
+            changeFont('Juliamo');
+            
+        } else {
+            changeFont('Meiryo');
+        }
+    }
 
-/*結果画面*/
-#resultDiv{
-    display: none;
-}
+    retryButton.onclick = function () {
+        
+    }
+});
 
-#message{
-    color:#49b460;
-    font-size:80px;
-    padding:100px 0 0;
-}
-
-#resultOption{
-    margin:100px 0 0;
-}
-
-#resultOption li{
-    margin:0 0 30px;
-}
-
-#resultOption input{
-    width:400px;
-    height:60px
-}
-
-#quizDiv{
-    display: none;
-}
