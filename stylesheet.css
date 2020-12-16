87483d08a28fb1723b9c8b7222bc9959066cb2fb
katacho <75933577+katacho@users.noreply.github.com>
2020-12-16 22:51:18 +0900

    Create stylesheet.css

---
+++ stylesheet.css
@@ -0,0 +1,186 @@
+@font-face{
+    font-family: 'Juliamo';
+    src: url('https://dl.dropboxusercontent.com/s/5me44aeb19mxm6e/juliamo.woff') format('woff');
+}
+
+button, input, select, textarea {
+    font-family : inherit;
+    letter-spacing: inherit;
+    font-size : 100%;
+}
+
+*{
+    color:#414141;
+    letter-spacing: 1px;
+}
+
+
+
+li{
+    list-style: none;
+    padding:0;
+}
+
+
+ul{
+    padding:0;
+    margin:0;
+}
+
+#mainContainer{
+    text-align: center;
+    margin:0 20px;
+    height:800px;
+}
+
+#topContainer{
+    height:100px;
+}
+
+#topContainer h1{
+    font-weight: bold;
+    font-size:50px;
+    line-height: 75px;
+    margin:0;
+    padding:20px 0 0;
+}
+
+#mainContainer h2{
+    font-weight: normal;
+    font-size:18px;
+    margin:0;
+    line-height: 27px;
+}
+
+#description h3{
+    border-bottom: 1px solid;
+    font-weight: normal;
+    font-size:16px;
+    margin:20px auto 10px;
+    line-height: 24px;
+    width:90px;
+}
+
+#description p{
+    margin:0 0 30px;
+    font-size:14px;
+    line-height: 21px;
+}
+
+#wordData{
+    width: 200px;
+    height: 200px;
+    padding: 5px;
+    border-radius: 5px;
+    font-size:14px;
+    line-height: 21px;
+    margin:0 auto;
+    display: block;
+}
+
+#startButton{
+    margin:10px 0 0;
+    height: 50px;
+    width:80px;
+    font-size:16px;
+    line-height: 24px;
+}
+
+#bottomContainer{
+    margin:0;
+    width:100%;
+    height:150px;
+}
+
+#checkBox{
+    padding:40px 0 0;
+    margin:0 0 20px;
+    font-size:20px;
+    height:30px;
+    line-height: 45px;
+}
+
+#dsc2{
+    font-size:14px;
+    line-height: 21px;
+}
+
+#quizContainer{
+    height:530px;
+    margin:0;
+}
+
+
+
+#quizText{
+    padding:100px 0 0;
+    margin:0;
+    font-size:50px;
+    line-height:50px;
+    height:50px;
+}
+
+#answerText{
+    margin:20px 0 50px;
+    font-size:40px;
+    height:40px;
+    visibility:hidden;
+}
+
+#answerButton{
+    box-sizing: border-box;
+    height:50px;
+    width:100px;
+    margin:0;
+}
+
+#inputBox{
+    height:60px;
+    width:200px;
+    font-size:30px;
+    line-height:30px;
+    margin:0;
+    border-right:none;
+    border-left:none;
+    border-top:none;
+    border-bottom: #929090 solid 1px;
+    outline: none;
+    transition-duration:0.1s;
+}
+
+#inputBox:focus{
+    border-bottom: #333333 solid 3px;
+}
+
+#checkIcons{
+    margin:30px 0 0;
+
+}
+
+/*結果画面*/
+#resultDiv{
+    display: none;
+}
+
+#message{
+    color:#49b460;
+    font-size:80px;
+    padding:100px 0 0;
+}
+
+#resultOption{
+    margin:100px 0 0;
+}
+
+#resultOption li{
+    margin:0 0 30px;
+}
+
+#resultOption input{
+    width:400px;
+    height:60px
+}
+
+#quizDiv{
+    display: none;
+}
